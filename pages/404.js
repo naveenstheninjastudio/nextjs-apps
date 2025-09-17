4e2e688166130d63e24b0cfffee415cf24ab5b41
naveenstheninjastudio <naveens@theninjastudio.com>
2025-09-17 15:12:20 +0530

    404 page add

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="notfound">
       <h1>oooops.....</h1>
-      <h2>that page cannot be found</h2>
+      <h2>requested page cannot be found</h2>
       <Link href="/">homepage</Link>
     </div>
   );
